e3b9ca25865f706e36e9769b3bba2b5a36a1c193
kithack <kithack@users.noreply.github.com>
2021-03-05 17:12:40 +0100

    Add arm64 release file. (#6)

--- make-releases.sh
+++ make-releases.sh
@@ -6,6 +6,7 @@ rm -rf build/*
 env GOOS=windows GOARCH=amd64 go build -o build/dyndns-netcup-go-windows.exe
 env GOOS=linux GOARCH=amd64 go build -o build/dyndns-netcup-go-linux
 env GOOS=linux GOARCH=arm go build -o build/dyndns-netcup-go-linux-arm
+env GOOS=linux GOARCH=arm64 go build -o build/dyndns-netcup-go-linux-arm64
 env GOOS=darwin GOARCH=amd64 go build -o build/dyndns-netcup-go-macos
 
 for file in ./build/*; do 
